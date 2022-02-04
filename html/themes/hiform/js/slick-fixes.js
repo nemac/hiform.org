9491b6da4b54aa5532260b8f03f1e709af353816
Jeff Bliss <jeff.a.bliss@gmail.com>
2022-02-04 19:31:24 +0000

    Drupal 9.3.4 commit

---
+++ html/themes/hiform/js/slick-fixes.js
@@ -0,0 +1,8 @@
+(function ($) {
+    $('.slick-dots').on('click', function () {
+	$('.autoplay').slick('slickPause');
+    });
+    $('.slick-arrow').on('click', function () {
+	$('.autoplay').slick('slickPause');
+    });
+})(window.jQuery);
